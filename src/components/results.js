228d5af985640f231035998d3cfcb1ee2644e5d9
Jonathan <jrugama96@gmail.com>
2021-06-09 12:15:50 -0600

    Se termina de realizar el componente de resultados, de la pregunta 1

--- src/components/results.js
+++ src/components/results.js
@@ -4,7 +4,27 @@ const Results = (props) => {
     return(
         <div className="results-container">
             <h2 className="results-title">Resultado</h2>
-            <p>{props.resultado}</p>
+            <table className="table-results">
+            <caption className="table-title">Cálculo de datos ingresados</caption>
+                <tbody className="table-body">
+                    <tr className="table-row">
+                        <td>Horas trabajadas</td>
+                        <td>{props.horasTrabajadas}</td>
+                    </tr>
+                    <tr className="table-row">
+                        <td>Tarifa horaria</td>
+                        <td>{props.tarifaHoraria}</td>
+                    </tr>
+                    <tr className="table-row">
+                        <td>Tasa de impuestos</td>
+                        <td>{props.tasaImpuestos}</td>
+                    </tr>
+                    <tr className="table-row">
+                        <td>Pago neto</td>
+                        <td>{props.resultado}</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
